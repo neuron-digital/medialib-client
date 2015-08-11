cd55bd131335613565ee380281dabcc00ff1a4a1
Denis Maximov <kst.skamm@gmail.com>
2015-08-11 13:31:43 +0300

    fix remove_column in Schema remove_attachment for Rails 3 compability
    While https://github.com/thoughtbot/paperclip/issues/1946 not fixed.

--- lib/medialib_client.rb
+++ lib/medialib_client.rb
@@ -6,6 +6,8 @@ require 'medialib_client/media_type_spoof_detector'
 require 'medialib_client/storage/media_lib'
 require 'medialib_client/interpolations'
 
+require 'medialib_client/schema'
+
 require 'medialib_client/engine'
 
 module MedialibClient
